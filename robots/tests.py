61918b00980328519ebf9b62d5f4218fa5f6c3a6
Pavel Zubko <tyyrok@gmail.com>
2023-09-25 13:44:53 +0400

    robots: add endpoint for adding single/multiple robots in db

--- robots/tests.py
+++ robots/tests.py
@@ -1,3 +1,79 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from robots.models import Robot
+import json
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        
+    def test_without_data_in_request(self):
+        response = self.client.post('/robots/add/', data={})
+        
+        self.assertEqual(response.status_code, 400)
+    
+    def test_with__empty_json_in_request(self):
+        response = self.client.post('/robots/add/', content_type="application/json", data={})
+        
+        self.assertEqual(response.status_code, 200)
+    
+    def test_with_incorrect_json_data(self):
+        data = "fd"
+        response = self.client.post('/robots/add/', content_type="application/json", data=data)
+        
+        self.assertEqual(response.status_code, 400)
+        
+    def test_with_incorrect_json_data_2(self):
+        data = { "model" : "R5"}
+        response = self.client.post('/robots/add/', content_type="application/json", data=data)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertGreater(len(json.loads(response.content)['failure']), 0)
+    
+    def test_with_correct_json_data(self):
+        data = {
+            "model": "R1",
+            "version": "D1",
+            "created": "2022-12-31 23:59:59"
+        }
+        response = self.client.post('/robots/add/', content_type="application/json", data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(json.loads(response.content)['success']), 1)
+        self.assertEqual(len(json.loads(response.content)['failure']), 0)
+        
+    def test_with_correct_multiple_json_data(self):
+        data = [
+            {
+                "model": "R5",
+                "version": "D2",
+                "created": "2022-12-31 23:59:59"
+            },
+            {
+                "model": "13",
+                "version": "XT",
+                "created": "2023-01-01 00:00:00"
+            },
+        ]
+        
+        response = self.client.post('/robots/add/', content_type="application/json", data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(json.loads(response.content)['success']), 2)
+        self.assertEqual(len(json.loads(response.content)['failure']), 0)
+        
+    def test_with_correct_multiple_json_data_with_incorrect_one(self):
+        data = [
+            {
+                "model": "R5",
+                "version": "D1",
+                "created": "2022-12-31 23:59:59"
+            },
+            {
+                "model": "13",
+                "version": "XSs",
+                "created": "2023-01-01 00:00:00"
+            },
+        ]
+        
+        response = self.client.post('/robots/add/', content_type="application/json", data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(json.loads(response.content)['success']), 1)
+        self.assertEqual(len(json.loads(response.content)['failure']), 1)
--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views import View
+from robots.forms import RobotForm
+import json
 
-# Create your views here.
+class RobotsAddView(View):
+    def post(self, request):
+        try:
+            raw_data = json.loads(request.body)
+        except Exception:
+            return JsonResponse({"detail": "Incorrect request"}, status=400)
+        
+        result_data = []
+        error_data = []
+        i = 0
+        if type(raw_data) != list:
+            obj = raw_data
+            raw_data = []
+            raw_data.append(obj)
+
+        for record in raw_data:
+            #record['']
+            form = RobotForm(data=record)
+            if form.is_valid():
+                obj = form.save(commit=False)
+                obj.serial = form.cleaned_data['model'] + '-' \
+                                            + form.cleaned_data['version']
+                result_data.append(form.cleaned_data)
+                obj.save()
+            else:
+                error_data.append({f"Record {i} - ": form.errors})
+            i += 1
+
+        return JsonResponse({'success': result_data, 
+                             'failure': error_data})
