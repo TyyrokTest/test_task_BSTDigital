9f492a8c24124ca0b150a4d71fb3c3119ac51693
Pavel Zubko <tyyrok@gmail.com>
2023-09-26 21:44:07 +0400

    task3: add functionality to make orders for robots and sending emails when robot in an order is getting available

--- orders/apps.py
+++ orders/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_save
 
 class OrdersConfig(AppConfig):
+    default_auto_field = "django.db.models.AutoField"
     name = 'orders'
+
+    def ready(self):
+        from .signals import send_notification_to_customers
+        
+        post_save.connect(send_notification_to_customers, sender="robots.Robot")
